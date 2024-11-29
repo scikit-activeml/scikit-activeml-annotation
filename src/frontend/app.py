b1082ccb2ec3537f0f19c56b1609205f8c35fa0b
NoCodeNoBlunder <findrunas@hotmail.com>
2024-11-29 11:04:01 +0100

    start working on backend

---
+++ src/frontend/app.py
@@ -0,0 +1,5 @@
+def main():
+    print("hello frontend")
+    
+if __name__ == '__main__':
+    main()
---
+++ src/util/path.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).parent.parent.parent
+CONFIG_PATH = ROOT_PATH / 'res' / 'config'
