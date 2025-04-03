580756769350f2e3297d2e774837f556298641d6
NoCodeNoBlunder <findrunas@hotmail.com>
2025-04-03 20:31:12 +0200

    feature: Add --prod flag to start the app in production mode

--- src/run.py
+++ src/run.py
@@ -7,26 +7,43 @@ from paths import (
     PROFILER_PATH
 )
 
+PORT = 8050
+
 
 def main():
     parser = argparse.ArgumentParser(description="Run the application with or without profiling.")
     parser.add_argument("--profile", action="store_true", help="Enable profiling mode")
+    parser.add_argument("--prod", action="store_true", help="Start the app in production mode")
     args = parser.parse_args()
 
     if args.profile:
-        # TODO Profiler should create a new dir with timestamp for the next log.
-        app.server.config["PROFILE"] = True
-        app.server.wsgi_app = ProfilerMiddleware(
-            app.server.wsgi_app,
-            sort_by=("cumtime", "tottime"),
-            restrictions=[20],
-            profile_dir=str(PROFILER_PATH)
-        )
-        print("Starting app in profiler mode")
-        app.run(debug=True, dev_tools_hot_reload=False)
+        run_profile_mode()
         return
+    elif args.prod:
+        run_prod_mode()
+    else:
+        app.run(debug=True, dev_tools_hot_reload_interval=1, port=PORT)
 
-    app.run(debug=True, dev_tools_hot_reload_interval=1)
+
+def run_profile_mode():
+    # TODO Profiler should create a new dir with timestamp for the next log.
+    app.server.config["PROFILE"] = True
+    app.server.wsgi_app = ProfilerMiddleware(
+        app.server.wsgi_app,
+        sort_by=("cumtime", "tottime"),
+        restrictions=[20],
+        profile_dir=str(PROFILER_PATH)
+    )
+    print("Starting app in profiler mode")
+    app.run(debug=True, dev_tools_hot_reload=False, port=PORT)
+
+
+def run_prod_mode():
+    import webbrowser
+    from waitress import serve
+
+    webbrowser.open(f"http://127.0.0.1:{PORT}/")
+    serve(app.server, host='127.0.0.1', port=PORT)
 
 
 if __name__ == "__main__":
