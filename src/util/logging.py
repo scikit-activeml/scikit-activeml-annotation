27f9015aee9949f05532a24a5afdccad151cbdbd
NoCodeNoBlunder <findrunas@hotmail.com>
2025-04-26 18:06:05 +0200

    improve: Set log level to info

--- src/util/logging.py
+++ src/util/logging.py
@@ -4,5 +4,6 @@ import logging
 def setup_logging():
     logging.basicConfig(
         format='[%(levelname)s] - %(message)s',
-        datefmt='%H:%M'  # This sets the time format to just hours and minutes
+        datefmt='%H:%M',  # This sets the time format to just hours and minutes
+        level=logging.INFO
     )
