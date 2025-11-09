3c078605d8c393d3935adf641091128597f5962d
Fabian Indrunas <findrunas@protonmail.com>
2025-11-09 20:46:36 +0100

    feat: add Log Mel Power Spectogram when displaying Audio Data

--- skactiveml_annotation/ui/pages/annotation/data_display.py
+++ skactiveml_annotation/ui/pages/annotation/data_display.py
@@ -1,15 +1,19 @@
+# TODO Todo Make it so not all dependencies for all modalities have to be installed
 import base64
 from pathlib import Path
 from io import BytesIO
 
-import dash
-from dash.html import Audio
 from dash import dcc
 import dash_player
 import dash_mantine_components as dmc
 
+import numpy as np
 from plotly import graph_objects as go
 
+# Audio libs
+import librosa
+import soundfile
+# Image libs
 from PIL import Image as pil_image
  
 from skactiveml_annotation.core.data_display_model import (
@@ -163,6 +167,7 @@ def create_text_display(path: Path, text_display_setting: TextDataDisplaySetting
 def create_audio_display(audio_data_path, audio_display_setting):
     # TODO this is a relative path must it not be made absolute?
 
+def create_audio_display(audio_data_path, audio_display_setting):
     """
     Creates a Dash Mantine AudioPlayer component from a local WAV file.
     
@@ -176,34 +181,128 @@ def create_audio_display(audio_data_path, audio_display_setting):
 
     print("Path", audio_data_path)
 
-    # Read and encode the audio file as base64
-    with open(audio_data_path, "rb") as f:
-        audio_bytes = f.read()
-    audio_b64 = base64.b64encode(audio_bytes).decode()
+    # TODO: create helper for base64 encoding
 
-    # Create the data URI
-    # TODO this will only work for wav files if the browser supports wav format
-    
-    # TODO create halper for this base64 encoding?
-    audio_src = f"data:audio/wav;base64,{audio_b64}"
+    # Load data from audio file as Pulse Code Modulation (PCM) timeseries 
+    # into numpy array.
+    # librosa uses soundfile and audiofile as a backup so all their file formats
+    # are supported
+    time_series, sample_rate = librosa.load(audio_data_path, sr=None) # Use native sampling rate
+
+    duration_in_sec = len(time_series) * (1 / sample_rate)
+
+    print("\n The duration of the sample in sec is:", duration_in_sec)
+    print("Sample Rate:", sample_rate)
+    print()
+
+    # Convert raw data into raw in memory representation of a wav file
+    # Create in memory represention of wav_file and write it into buffer
+    wav_file_bytes_buffer = BytesIO()
+    soundfile.write(file=wav_file_bytes_buffer, data=time_series, samplerate=sample_rate, format="WAV")
+        
+    # Encode in memory wav_file as base64 so it works in the browser as inline data URL.
+    wav_file_b64_enc_bytes = base64.b64encode(wav_file_bytes_buffer.getvalue()).decode()
+
+    # Pretty much all browsers support wav format: https://caniuse.com/wav
+    mime = "audio/wav"
+    inline_wave_file_url = f"data:{mime};base64,{wav_file_b64_enc_bytes}"
+
+    player = dash_player.DashPlayer(
+        url=inline_wave_file_url,
+        controls=True,
+        loop=audio_display_setting.loop,
+        playbackRate=audio_display_setting.playback_rate,
+        height=50,
+    )
+
+    fig = create_log_mel_power_spectogramm_fig(time_series, sample_rate)
 
     # Create the Dash Mantine AudioPlayer
     return dmc.Center(
-        dash_player.DashPlayer(
-            url=audio_src,
-            controls=True,
-            loop=audio_display_setting.loop,
-            playbackRate=audio_display_setting.playback_rate,
-            height=50,
+        dmc.Stack(
+            [
+                dcc.Graph(figure=fig),
+                player
+            ]
         ),
-        # Audio(
-        #     title="test",
-        #     src=audio_src,
-        #     controls=True,
-        #     autoPlay='autoplay',
-        #     muted=False,
-        #     loop=True,
-        # ),
         m="xl"
     )
 
+
+def create_log_mel_power_spectogramm_fig(
+    time_series, 
+    sample_rate, 
+    n_fft=4096,
+    hop_length=256, 
+    n_mels=128,
+    fmin=80,
+    fmax=8000
+):
+    fmin = fmin
+    # fmax should never exeed Nyhilist limit
+    fmax = min(sample_rate / 2, fmax) 
+
+    # Frequency axis
+    S = librosa.feature.melspectrogram(
+        y=time_series, 
+        sr=sample_rate, 
+        n_fft=n_fft, 
+        hop_length=hop_length, 
+        n_mels=n_mels, 
+        center=True,
+        fmin=fmin,
+        fmax=fmax
+    )
+    S_db = librosa.power_to_db(S, ref=np.max)
+    # Center of each mel frequency bin in regular Hz
+    freqs = librosa.mel_frequencies(n_mels=S.shape[0], fmin=fmin, fmax=fmax)
+    # Mel frequencies are not equy distant. Lower frequencies get stretched apart
+    # and higher frequencies get squished closer together
+    mel_freq = librosa.hz_to_mel(freqs)
+
+    times = librosa.frames_to_time(np.arange(S_db.shape[1]), sr=sample_rate, hop_length=hop_length)
+
+    tick_idxs = np.arange(0, len(freqs), 32)
+    tickvals = mel_freq[tick_idxs]
+    ticktext = [f"{int(freqs[i])}" for i in tick_idxs]
+
+    # Create heatmap
+    fig = go.Figure(
+        data=go.Heatmap(
+            z=S_db,
+            x=times,
+            y=mel_freq,
+            colorscale="magma",
+            colorbar=dict(title="dB"),
+            zmin=S_db.min(),
+            zmax=S_db.max(),
+        )
+    )
+
+    axis_args = dict(
+        showticklabels=True,
+        ticks="outside",
+        ticklen=6,
+        tickwidth=1.5,
+        showline=True,
+        mirror=True,
+        tickcolor="black",
+        linecolor="black",
+    )
+
+    # Configure axes
+    fig.update_layout(
+        title_text="Log Mel Power Spectrogram",
+        title_x = 0.5,
+        xaxis_title="Time (s)",
+        yaxis_title="Frequency (Hz)",
+        xaxis=dict(**axis_args),
+        yaxis=dict(
+            tickmode="array", # Specify that it should use tickvals to determine tick positions
+            tickvals=tickvals,
+            ticktext=ticktext,
+            **axis_args
+        )
+    )
+
+    return fig
